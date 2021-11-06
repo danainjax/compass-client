7edf191d455c70ef3e41a1c611d2f128f8ac6ba2
danainjax <danainjax@gmail.com>
2021-11-06 09:52:00 -0400

    displaying json to the page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,24 @@
+import React, { useState, useEffect } from 'react'
+
 const Project = () => {
-  fetch('http://localhost:3000/projects')
-    .then((response) => response.json())
-    .then((projects) => console.log(projects))
+  const [projects, setProjects] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/projects')
+      .then((response) => response.json())
+      .then(setProjects)
+  }, [])
+  console.log(projects)
 
   return (
     <>
       <h1>Projects</h1>
-      <ul>{/* <li>{project.title}</li> */}</ul>
+      <ul>
+        <li>
+          <p>Project title:{projects[0].title}</p>
+          <p> Project id:{projects[0].id}</p>
+        </li>
+      </ul>
     </>
   )
 }
